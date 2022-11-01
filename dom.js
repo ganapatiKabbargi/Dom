d1f10eddfa856473f0d8eabc39082133724a4a26
ganapatiKabbargi <ganapatisk96@gmail.com>
2022-11-01 19:23:46 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,5 @@
+const headertitleEl=document.getElementById("header-title");
+const titleEl=document.querySelector(".container h2")
+headertitleEl.style.borderBottom="4px solid black"
+titleEl.style.color="green"
+titleEl.style.fontWeight="bold"
