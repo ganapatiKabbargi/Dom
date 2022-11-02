e5f59859890984722f6150197e1ecf57866f924a
ganapatiKabbargi <ganapatisk96@gmail.com>
2022-11-02 11:46:47 +0530

    get element by tag name

--- dom.js
+++ dom.js
@@ -1,8 +1,15 @@
-var itemsEl=document.getElementsByClassName("list-group-item");
+// var itemsEl=document.getElementsByClassName("list-group-item");
 
-itemsEl[1].style.backgroundColor="green";
+// itemsEl[1].style.backgroundColor="green";
 
-for(let item of itemsEl){
-    item.style.fontWeight="bold"
+// for(let item of itemsEl){
+//     item.style.fontWeight="bold"
+//     item.style.color="red"
+// }
+
+var listItem=document.getElementsByTagName("li");
+
+for(let item of listItem){
     item.style.color="red"
+    item.style.backgroundColor="lightgrey"
 }
